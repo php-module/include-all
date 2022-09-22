7659699d0e6bd7a535d6daff4dc991af25a0d6b2
Samil <agostinhosaml832@gmail.com>
2022-09-22 18:46:11 +0100

    root: initial commit

---
+++ src/Sammy/Packs/IncludeAll.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @version 2.0
+ * @author Sammy
+ *
+ * @keywords Samils, ils, php framework
+ * -----------------
+ * @namespace Sammy\Packs
+ * - Autoload, application dependencies
+ */
+namespace Sammy\Packs {
+  #use Sammy\Packs\IncludeAll\Base as IncludeAllBaseClass;
+  /**
+   * Make sure the module base internal class is not
+   * declared in the php global scope defore creating
+   * it.
+   */
+  class IncludeAll extends IncludeAll\Base {
+  }
+}
