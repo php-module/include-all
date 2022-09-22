c4e6fae22725eafba9693e1cc6fed2a693165154
Samil <agostinhosaml832@gmail.com>
2022-09-22 18:45:46 +0100

    root: create pack autoload files

---
+++ autoload.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @version 2.0
+ * @author Sammy
+ *
+ * @keywords Samils, ils, php framework
+ * -----------------
+ * @namespace Sammy\Packs\IncludeAll
+ * - Autoload, application dependencies
+ */
+namespace Sammy\Packs\IncludeAll {
+	defined('INCLUDE_ALL_ROOT') or define (
+    'INCLUDE_ALL_ROOT', __DIR__
+  );
+	/**
+	 * Make sure the module base internal class is not
+	 * declared in the php global scope defore creating
+	 * it.
+	 */
+	$includeAll = require (dirname (__FILE__) . '/src/index.php');
+	/**
+	 * Autoload includeAll extensions
+	 */
+	$includeAll->includeAll ('./src');
+}
